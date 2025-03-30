43fdfbf49205d7df0ad5a252415d55ab0ad341c6
sammelkorn <sammelkorn@debian490.490debs>
2025-03-30 18:47:56 +0200

    refactor: write_to_usage.db

--- write_to_usage_db.py
+++ write_to_usage_db.py
@@ -2,8 +2,6 @@ import sqlite3
 from datetime import datetime
 import csv
 
-# TODO determine start date somehow, eliminate lines without data
-# TODO
 # create table
 conn = sqlite3.connect("usage_data.db")
 cursor = conn.cursor()
@@ -25,11 +23,7 @@ header = next(usage_data)
 for row in usage_data:
     date = row[0]
     date = datetime.strptime(date, "%d.%m.%Y").strftime("%Y-%m-%d")
-    # Fetch data and convert from comma ...
     usage = row[1]
-    # print(f"pre-con: {type(date)}: {type(usage)}")
-    # print(f"pre-con: {date}: {usage}")
-    # print(f"type before treatment {type(usage)}")
     if usage:
         usage = float(usage.replace(",", "."))
         print(f"stored in db: {date}: {usage}kWh")
