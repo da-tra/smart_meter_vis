40c9288d985b4a2c042c117daaa52bbd8312e608
sammelkorn <sammelkorn@debian490.490debs>
2025-03-30 21:10:54 +0200

    refactor continued: changing to one sql table

--- write_to_usage_db.py
+++ write_to_usage_db.py
@@ -2,6 +2,11 @@ import sqlite3
 from datetime import datetime
 import csv
 
+# API Details
+with open("api_key.txt", "r") as f:
+    API_KEY = f.read().strip()
+LAT, LON = 48.2083537, 16.3725042
+
 # create table
 conn = sqlite3.connect("usage_data.db")
 cursor = conn.cursor()
@@ -45,6 +50,17 @@ for row in usage_data:
     # print(row[1])
 
 conn.commit()
+# TODO add new columns and data here
+cursor.execute("SELECT date FROM usage WHERE retrieval_date IS NULL OR retrieval_date = ''")
+rows = cursor.fetchall()
 
+# Step 3: Process each row
+for row in rows:
+    stored_date = row[0]  # Extract date from query result
+
+    print(f"Fetching data for {stored_date}...")
+
+    # Step 4: Make API call using the stored date
+    url = f"https://api.openweathermap.org/data/3.0/onecall/day_summary?lat={LAT}&lon={LON}&date={date}&appid={API_KEY}"
 csv_usage.close()
 
