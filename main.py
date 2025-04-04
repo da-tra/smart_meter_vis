3e2ca6a19e4dbaaedd9f5f251b1174f4fca51c47
David Traska <sammelkorn@Davids-MacBook-Pro.local>
2025-04-04 21:22:07 +0200

    refactor and comment: writing data from csv to sql

--- main.py
+++ main.py
@@ -50,13 +50,13 @@ CREATE TABLE IF NOT EXISTS {table_name} (
     {columns_block}
 )
 """
+# Execure the query and commit the results to the database
 cursor.execute(query)
-
 conn.commit()
 
-# Write usage data from CSV object to SQL table
+# Write usage data from CSV object to the newly created SQL table
 for row in usage_data:
-    # Get date from first column 
+    # Get date from first column and format to YYYY-MM-DD
     date_csv = row[0]
     date_csv = datetime.strptime(date_csv, "%d.%m.%y")
     date_csv = date_csv.strftime("%Y-%m-%d")
@@ -64,27 +64,21 @@ for row in usage_data:
     # Get power consumption from second column
     usage = row[1]
 
-    # TODO change from printing to logging:
-
-    # If there is data the csv:
-    #   1) change format from 1,12 to 1.23
-    #   2) print successful storage
-    # Otherwise:
+    # If the current row of the usage csv is empty:
     #   i) report missing data
-    #   ii) Leave loop, work to next row
-
-    if usage:
-        usage = float(usage.replace(",", "."))
-        print(f"Stored in DB: {date_csv}: {usage} kWh")
-    else:
+    #   ii) leave loop, work to next row
+    if not usage:
         print(f"Skipped: {date_csv} (No data)")
         continue
+    # Otherwise: reformat usage data from 1,12 to 1.23
+    else:
+        usage = float(usage.replace(",", "."))
 
-    # (ctd.) If there is data in the csv:
-    #   3) check if the same data already exists in the sql table
+    # Only write this day's usage data to the sql table if there is no data for it
+    #   Query the sql table for data for the date from the csv
     query = f"SELECT usage_kwh FROM {table_name} WHERE date = ?"
     cursor.execute(query, (date_csv,))
-
+    #   Examine the result for content
     rows_sql = cursor.fetchone()
 
     if not rows_sql:
@@ -95,20 +89,20 @@ for row in usage_data:
             print(f"{usage} on {date_csv}: {row} (Already in database)")
         # restart loop
         continue
-    #   4) write row sql table using .format()
-    query = """
-        INSERT INTO {} (date, usage_kwh)
-        VALUES (?, ?)
-    """.format(table_name)  # Use format only for table name
+    #   Write row to sql table
+    query = f"""
+            INSERT INTO {table_name} (date, usage_kwh)
+            VALUES (?, ?)
+            """  # noqa: S608
 
     cursor.execute(query, (date_csv, usage))  # Pass values safely
 conn.commit()
 conn.close()
 
 
-# The next section 
-# 1) Defines details for an API GET call to Openweathermap.org
-# 2) Adds new columns for the expected data shape to the SQL table power_usage_vs_weather
+# The next section...
+# 1) defines details for an API GET call to Openweathermap.org
+# 2) adds new columns for the expected data shape to the SQL table power_usage_vs_weather
 
 # API Details: Get API key for weather app
 with open("api_key.txt", "r") as f:
@@ -117,7 +111,7 @@ with open("api_key.txt", "r") as f:
 LAT, LON = 48.2083537, 16.3725042
 
 # Limit costs by limiting API call  
-API_CALL_LIMIT = 900  # Change this number as needed
+API_CALL_LIMIT = 9  # Change this number as needed
 
 # Define new columns with their respective types
 columns = {
